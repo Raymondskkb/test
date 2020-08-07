09fc8ebca3b690e46f33b84b4c8e04fd326784b2
Raymondskkb <raymondskkb@gmail.com>
2020-08-07 21:21:41 +0300

    comment

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 #Loop through numbers from 1 to 100
 for i in range(1, 101):
-    #If number is a multiple of 3 print Fizz, if a mulitiple of 5 print Buzz, if a multiple of 
-    #both 3 and 5 print FizzBuzz, otherwise print the number
+    '''If number is a multiple of 3 print Fizz, if a mulitiple of 5 print Buzz, if a multiple of 
+       both 3 and 5 print FizzBuzz, otherwise print the number
+    '''
     if i % 15 == 0:
         print("FizzBuzz")
     elif i % 3 == 0: #if number is divisible by 5 print buzz
