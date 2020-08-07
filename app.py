04f87d22543894c5fc22eec7c883699f23706bf8
Raymondskkb <raymondskkb@gmail.com>
2020-08-07 19:06:39 +0300

    added comments

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ for i in range(1, 101):
     #both 3 and 5 print FizzBuzz, otherwise print the number
     if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
-    elif i % 3 == 0:
+    elif i % 3 == 0: #if number is divisible by 5 print buzz
         print("Fizz")
-    elif i % 5 == 0:
+    elif i % 5 == 0: #if number is divisible by 5 print buzz
         print("Buzz")
     else:
         print(i)
