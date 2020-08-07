e8f0218bda2cf775e7c08f4ff904b1c05c340909
Raymondskkb <raymondskkb@gmail.com>
2020-08-07 20:10:36 +0300

    changed line 5

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 for i in range(1, 101):
     #If number is a multiple of 3 print Fizz, if a mulitiple of 5 print Buzz, if a multiple of 
     #both 3 and 5 print FizzBuzz, otherwise print the number
-    if i % 3 == 0 and i % 5 == 0:
+    if i % 15 == 0:
         print("FizzBuzz")
     elif i % 3 == 0: #if number is divisible by 5 print buzz
         print("Fizz")
